6f9c585b80a29d18e96239132caa2e96b167af56
Naphat Permpredanun <68365175+NaphatPRM@users.noreply.github.com>
2025-06-27 12:13:47 +0700

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -4,8 +4,8 @@ import json
 def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
 	#YOUR CODE HERE
-  json_string = json.dumps(data, indent=4)
-  api_key = "b4acda270bd68f14cc01"
+	json_string = json.dumps(data, indent=4)
+	api_key = "b4acda270bd68f14cc01"
   api_secret = "e031547ad0f877c659cd2db7c1d5c9b42dad504bd5fbe996d726298c22b7123b"
   jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI5MzgxMzFiZS00ZTQwLTRiZDctYmQzMy0wNzEwNTc1MmZkMTQiLCJlbWFpbCI6ImF1dG5hcGhhdEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJGUkExIn0seyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJOWUMxIn1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiYjRhY2RhMjcwYmQ2OGYxNGNjMDEiLCJzY29wZWRLZXlTZWNyZXQiOiJlMDMxNTQ3YWQwZjg3N2M2NTljZDJkYjdjMWQ1YzliNDJkYWQ1MDRiZDVmYmU5OTZkNzI2Mjk4YzIyYjcxMjNiIiwiZXhwIjoxNzgyNTMyMTA2fQ.UHQqS1s-uU79KSn-4-wnZ_7NQ-t7yz09Q3TdDmy9hoo"
 
