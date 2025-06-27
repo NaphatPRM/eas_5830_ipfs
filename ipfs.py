fbd3ae98f47053739835256cc7261d32971d57d5
Naphat Permpredanun <68365175+NaphatPRM@users.noreply.github.com>
2025-06-27 17:54:10 +0700

    Update using Python

--- ipfs.py
+++ ipfs.py
@@ -1,62 +1,41 @@
 import requests
 import json
+import io
+
+jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI5MzgxMzFiZS00ZTQwLTRiZDctYmQzMy0wNzEwNTc1MmZkMTQiLCJlbWFpbCI6ImF1dG5hcGhhdEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJGUkExIn0seyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJOWUMxIn1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiYjRhY2RhMjcwYmQ2OGYxNGNjMDEiLCJzY29wZWRLZXlTZWNyZXQiOiJlMDMxNTQ3YWQwZjg3N2M2NTljZDJkYjdjMWQ1YzliNDJkYWQ1MDRiZDVmYmU5OTZkNzI2Mjk4YzIyYjcxMjNiIiwiZXhwIjoxNzgyNTMyMTA2fQ.UHQqS1s-uU79KSn-4-wnZ_7NQ-t7yz09Q3TdDmy9hoo"
 
 def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
 	#YOUR CODE HERE
 	json_string = json.dumps(data, indent=4)
-	api_key = "b4acda270bd68f14cc01"
-	api_secret = "e031547ad0f877c659cd2db7c1d5c9b42dad504bd5fbe996d726298c22b7123b"
-	jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI5MzgxMzFiZS00ZTQwLTRiZDctYmQzMy0wNzEwNTc1MmZkMTQiLCJlbWFpbCI6ImF1dG5hcGhhdEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJGUkExIn0seyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJOWUMxIn1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiYjRhY2RhMjcwYmQ2OGYxNGNjMDEiLCJzY29wZWRLZXlTZWNyZXQiOiJlMDMxNTQ3YWQwZjg3N2M2NTljZDJkYjdjMWQ1YzliNDJkYWQ1MDRiZDVmYmU5OTZkNzI2Mjk4YzIyYjcxMjNiIiwiZXhwIjoxNzgyNTMyMTA2fQ.UHQqS1s-uU79KSn-4-wnZ_7NQ-t7yz09Q3TdDmy9hoo"
+	json_bytes = json_string.encode("utf-8")
+	file_setting = io.BytesIO(json_bytes)
 
-	try {
-		const formData = new FormData();
+  # Construct the multipart/form-data payload
+	files = {'file': ('testing.json', file_setting, 'application/json')}
+	data = {'network': 'public'}
+	headers = {'Authorization': f'Bearer {jwt}'}
 
-		const blob = new Blob([json_string]);
-		const file = new File([blob], "testing.json", { type: "application/json" });
+  # Send the POST request
+	response = requests.post('https://uploads.pinata.cloud/v3/files', headers=headers, files=files, data=data)
+	response.raise_for_status()  # Raise an error for bad responses
 
-		formData.append("file", file);
-
-		formData.append("network", "public");
-
-		const request = await fetch("https://uploads.pinata.cloud/v3/files", {
-			method: "POST",
-			headers: {
-				Authorization: `Bearer ${jwt}`,
-			},
-			body: formData,
-		});
-		const response = await request.json();
-		return response.id
-	} catch (error) {
-		console.log(error);
-	}
-
-	return cid
+  # Parse response and return the CID or file ID
+	return response.json().get('id')
 
 def get_from_ipfs(cid,content_type="json"):
 	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
 	#YOUR CODE HERE	
-	try {
-		const payload = json.stringify({
-			url: "https://example.mypinata.cloud/files/" + cid,
-			expires: 500000,
-			method: "GET"
-		})
+	# Send the GET request
+	url = "https://copper-changing-roadrunner-585.mypinata.cloud/ipfs/" + cid
+	headers = {'Authorization': f'Bearer {jwt}'}
 
-		const request = await fetch(
-			"https://api.pinata.cloud/v3/files/download_link",
-		{
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${jwt}`,
-			},
-			body: payload
-		});
-		const data = await request.json();
-		assert isinstance(data,dict), f"get_from_ipfs should return a dict"
-		return data
-	} catch (error) {
-		console.log(error);
-	}
+	data = requests.get(url, headers=headers)
+	data.raise_for_status()  # Raise an error for bad responses
+
+	data = data.json()
+	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
+	return data
+
+if __name__ == "__main__":
+	get_from_ipfs("bafkreiaqxgprbh35ef4labvcxcukjpnqh7blzuxzwyu3rmsjs6ee4h3y4i")
